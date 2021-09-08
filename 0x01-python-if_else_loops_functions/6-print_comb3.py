f398a2f212c3375a41d63e8a3650f26dab53c423
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-08 09:51:28 +0000

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#Print any posible combination without reapeting same digits from before
+for number in range(0, 9 + 1):
+    for number2 in range(0, 9 + 1):
+        if number < number2:
+            print("{:d}{:d}".format(number, number2), end='')
+            if number is not 8:
+                print(", ", end="")
+print('')
