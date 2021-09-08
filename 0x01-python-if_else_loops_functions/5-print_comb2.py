0e131e029ba9844817fc5a5f8ca9a019d9ba4c58
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-08 16:39:07 +0000

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# Print numbers from 0 to 99
 for number in range(0, 99 + 1):
     print("{:02d}".format(number), end="")
-print('99')
-#    if number is not 99:
-#        print(", ", end='')
-#print("")
+#print('99')
+    if number is not 99:
+        print(", ", end='')
+print("")
 # print(", ".join("{:02d}".format(number) for number in range(0, 99 + 1)))
 # Using .join will add a separator between each iterable item
