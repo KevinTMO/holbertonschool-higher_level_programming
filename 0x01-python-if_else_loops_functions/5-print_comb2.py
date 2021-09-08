cc87ffe82ea9e60c9194756200a92f0a4e69ace2
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-08 16:46:41 +0000

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for number in range(0, 99 + 1):
-    print("{:02d}, ".format(number), end="")
-print('99')
+print(", ".join("{:02d}".format(number) for number in range(0, 99 + 1)))
