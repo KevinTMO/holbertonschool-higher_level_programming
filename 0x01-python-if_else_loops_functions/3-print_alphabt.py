e5598fa96839d768d7245ad562293ddc8b66d97b
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-08 11:04:17 +0000

    Fix pep8

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#Print alphabet without q and e in lowercase
+# Print alphabet without q and e in lowercase
 for alpha in range(ord('a'), ord('z') + 1):
     if alpha is not ord('e') and alpha is not ord('q'):
-    #if alpha != ord('e') and alpha != ord('q') works too
+        # if alpha != ord('e') and alpha != ord('q') works too
         print("{:s}".format(chr(alpha)), end='')
