3e50ea01dee840cb5cdb555a9fd7ed338593c53a
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-13 17:19:12 +0000

    Task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__name__":
-
-    import variable_load_5
-
-    var = variable_load_5.a
-
-    print(var)
+    from variable_load_5 import a
+    print("{}".format(a))
