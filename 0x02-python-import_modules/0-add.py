e609b1851015f5defad1ce7980899dbf879d0477
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-13 16:35:36 +0000

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # Creating a function that call another function for addition
 
-from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
+    from add_0 import add
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)), end="")
-print()
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
