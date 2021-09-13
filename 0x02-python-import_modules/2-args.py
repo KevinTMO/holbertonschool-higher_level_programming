f50d9aa6fb8188b902dd8bb11c0ba469c010b7ef
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-13 10:38:52 +0000

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Import argv to iterate arguments
+
+if __name__ == "__main__":
+
+    from sys import argv
+
+    input_usr = argv[1:]
+    input_size = len(input_usr)
+
+    print("{:d} {:s}{:s}".format(input_size, "arguments" if input_size is not 1
+                                 else "argument", ":" if input_usr is not 0
+                                 else "."))
+
+    for index_args, args in enumerate(input_usr):
+        print("{:d}: {:s}".format(index_args + 1, args))
