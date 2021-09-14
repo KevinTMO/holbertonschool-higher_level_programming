c7638fa52599e61c6d6d729bf652f0c177619be5
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-14 07:26:43 +0000

    Task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Print a list of integers
+
+
+def print_list_integer(my_list=[]):
+    """
+    Will print a list of integers
+    my_list: is the list of integers to prin
+    """
+#    newList = my_list <- testing with another variable
+    print("{}".format(my_list))
