10132256706da1865d227b8a03d6b7842ea404ea
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-14 13:08:19 +0000

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -7,4 +7,6 @@ def multiple_returns(sentence):
     Will return the len and first chr of a sentence
     sentence: is the string to get the lenght and first chr
     """
-    return (len(sentence), sentence[0])
+    lenStr = len(sentence)
+
+    return (lenStr, sentence[0] if lenStr > 0 else None)
