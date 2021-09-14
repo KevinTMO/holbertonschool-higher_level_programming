9373b78619c53b4b598c01595e0af35058605c97
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-14 13:34:54 +0000

    Task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Delete a value at pos
+
+
+def delete_at(my_list=[], idx=0):
+    """
+    Will delete an item at idx pos
+    my_list: is a list of integers
+    idx: is the pos to delete
+    """
+    if idx < 0 or idx > len(my_list):
+        return my_list
+
+    else:
+        del(my_list[idx])
+        return my_list
