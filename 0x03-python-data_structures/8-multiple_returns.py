74fdc39cec50c6257890e88cb6d8f6ba0a24437d
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-14 12:47:11 +0000

    Task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Return lenght and first char of a sentence
+
+
+def multiple_returns(sentence):
+    """
+    Will return the len and first chr of a sentence
+    sentence: is the string to get the lenght and first chr
+    """
+    return (len(sentence), sentence[0])
