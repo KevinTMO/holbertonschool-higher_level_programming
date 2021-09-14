4a3c13e0cf008910024b592daf650a48ea517b3c
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-14 11:20:10 +0000

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Print a matrix of integers
+
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    Will print a matrix of integers
+    matrix: is the matrix to print
+    """
+    for newMatrix in matrix:
+        print(" ".join(map(str, newMatrix)))
