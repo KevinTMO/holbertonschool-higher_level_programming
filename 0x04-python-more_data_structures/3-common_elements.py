3e554c17d61ec4fcf838e2f658470d3a148241ad
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-16 20:52:24 +0000

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -7,8 +7,8 @@ def common_elements(set_1, set_2):
     set_1: first set of items
     set_2: second set of items
     """
-    if set_1 and set_2:
+    if set_1 is not None and set_2 is not None:
         return set_1 & set_2
 
     else:
-        return
+        return None
