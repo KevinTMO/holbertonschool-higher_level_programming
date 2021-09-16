ed2ef666a873f8a48ad39f9eedcfb479fab40359
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-16 20:49:55 +0000

    Task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -6,7 +6,4 @@ def uniq_add(my_list=[]):
     """
     my_list: a list of integers
     """
-    if my_list:
-        newList = my_list.copy()
-
-    return sum(set(newList))
+    return sum(set(my_list))
