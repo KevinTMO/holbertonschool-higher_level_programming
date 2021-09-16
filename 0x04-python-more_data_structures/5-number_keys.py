d287669b3a1384dbffe995812be93ebfc3ce52a9
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-16 12:25:35 +0000

    Task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Return the number of keys in a dictionary
+
+
+def number_keys(a_dictionary):
+    """
+    a_dictionary: is the dictionary with key, values
+    """
+    return len(a_dictionary.keys())
