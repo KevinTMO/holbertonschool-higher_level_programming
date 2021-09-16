aa6c065a3ff018d0ef22e1085e889d4752412abf
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-16 20:51:19 +0000

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -7,7 +7,7 @@ def common_elements(set_1, set_2):
     set_1: first set of items
     set_2: second set of items
     """
-    if len(set_1) != 0 or len(set_2) != 0:
+    if set_1 and set_2:
         return set_1 & set_2
 
     else:
