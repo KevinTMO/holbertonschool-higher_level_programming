8cf35f49a87c11bc1266feb4d654d52d705854fa
KevinTMO <kevin.ramos3@hotmail.com>
2021-09-16 20:55:58 +0000

    Task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-# Return mapped values to a new list
-
-
-
 def multiply_list_map(my_list=[], number=0):
-    """
-    my_list: a list of integers
-    number: to value to multiply each integer
-    """
-    if my_list:
-        return list(map(lambda value: value * number, my_list))
+    return list(map(lambda value: value * number, my_list))
