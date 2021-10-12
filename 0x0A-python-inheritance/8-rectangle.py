5277bd6a3477ef90d0ec78fbffd576fe497fca6b
KevinTMO <kevin.ramos3@hotmail.com>
2021-10-12 12:35:20 +0000

    Creating another class that inherit from a superclass

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module: 8-rectangle
+
+Method:
+- def __init__(self, width, height)
+  - Instantiation with super() to inherit from:
+  ... base_geometry class.
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    Inherit from BaseGeometry methods
+    """
+    def __init__(self, width, height):
+        """
+        Using super() to call a method from BaseGeometry to validate
+        Instantiation of width && height
+        width: private
+        height: private
+        """
+        super().integer_validator("width", width) # sending width name and val
+        self.__width = width # giving result
+        super().integer_validator("height", height) #sending height name and val
+        self.__height = height # giving result
