808efd2600c586b3e05b7dbe0cffde8c7bc0e473
KevinTMO <kevin.ramos3@hotmail.com>
2021-10-19 22:14:41 +0000

    Adding to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,6 +7,9 @@ Class: Base
 """
 
 
+import json
+
+
 class Base:
     """
     Creating a class attrs there's no id
@@ -16,10 +19,21 @@ class Base:
     def __init__(self, id=None):
         """
         if id is something (not NONE) then assign attr id
-        else just increment nb_object
+        else just increment nb_object (number of objects)
         """
         if id:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    #  json methods
+
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        else:
+            list_dictionaries = []
