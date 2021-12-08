bdc2872761bea523f4368e561aa235754361543c
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 11:13:18 +0000

    Fixing args way to use it

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
 from sys import stderr
 
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
-        return result
+        # result = fct(*args)
+        # return result
+        return fct(*args)
     except (ZeroDivisionError, IndexError, TypeError, ValueError) as ve:
         stderr.write("Exception: {}\n".format(ve))
         return None
