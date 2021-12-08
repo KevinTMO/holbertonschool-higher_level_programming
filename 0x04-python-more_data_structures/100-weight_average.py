4dee9143d4c6409f27d7f237378abcf811e75e98
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 12:15:17 +0000

    Adding a new condition to the if statement

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     """
     return the average weight of all ints inside every tuple in the list
     """
-    if len(my_list) == 0:
+    if len(my_list) == 0 or my_list is None:
         return None
 
     return(sum(first * second for first, second in my_list)
