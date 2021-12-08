7f7a30451c03d9c5aed410481861b38f2a545bc9
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 07:08:30 +0000

    Create a list that contains all the attrs you want to use

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-
 """
 Module: 101-locked_class.py
 
 Prevents the user from dynamically creating a new instance attr
 Except when calling "first_name"
-
 """
 
 
-class LockedClass:
+class LockedClass():
     """
     Defining a list with all attrs you want to use
     Anything not in the list cannot be used as attrs
