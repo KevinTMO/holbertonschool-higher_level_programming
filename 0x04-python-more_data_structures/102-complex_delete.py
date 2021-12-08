bb175ce7c27ae9ef3d032fc2fd4521fad867e3bb
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 15:40:58 +0000

    Using old logic instead of copying dict

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    cpyDict = a_dictionary.copy()
     for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
-            del cpyDict[key]
+            del a_dictionary[key]
 
-    return cpyDict
+    return a_dictionary
