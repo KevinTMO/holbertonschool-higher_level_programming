2ba2d06976212d44addc2a7d366b1e61641a9b46
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 12:20:04 +0000

    Changing conditions and fixing return if len is 0 of the list to zero

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,8 +3,13 @@ def weight_average(my_list=[]):
     """
     return the average weight of all ints inside every tuple in the list
     """
-    if len(my_list) == 0 or my_list is None:
-        return None
+    if len(my_list) == 0:
+        return 0
 
-    return(sum(first * second for first, second in my_list)
-           / sum(second for first, second in my_list))
+    if my_list:
+        return(sum(first * second for first, second in my_list)/sum(second
+                                                                    for first,
+                                                                    second
+                                                                    in my_list))
+    else:
+        return 0
