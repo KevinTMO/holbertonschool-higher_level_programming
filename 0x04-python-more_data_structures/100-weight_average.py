7a543400f9b6592e55eba1c16ac4fa212b9af47b
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 12:09:23 +0000

    Average weight

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """
+    return the average weight of all ints inside every tuple in the list
+    """
+    if len(my_list) == 0:
+        return None
+
+    return(sum(first * second for first, second in my_list)
+           / sum(second for first, second in my_list))
