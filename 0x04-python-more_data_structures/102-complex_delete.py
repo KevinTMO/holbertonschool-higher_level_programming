943a0f64ff912abf4bbc2f2d802d9b419b11c6e1
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 15:39:22 +0000

    Removing statements

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if a_dictionary:
-        for key in list(a_dictionary.keys()):
-            if a_dictionary[key] == value:
-                del a_dictionary[key]
+    cpyDict = a_dictionary.copy()
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del cpyDict[key]
 
-        return a_dictionary
+    return cpyDict
