0d18b39dd2c02495f9f865e83ba9a204bf6c38ba
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 15:35:52 +0000

    Deleting the value a of a key in a dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if len(a_dictionary) == 0:
-        return None
-
     if a_dictionary:
         for key in list(a_dictionary.keys()):
             if a_dictionary[key] == value:
