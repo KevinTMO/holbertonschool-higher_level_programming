e0459452e0d65fef5ea69964ef3a801e2f4a9d6c
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-08 07:04:32 +0000

    Create a list that contains all the attrs you want to use

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Module: 101-locked_class.py
+
+Prevents the user from dynamically creating a new instance attr
+Except when calling "first_name"
+
+"""
+
+class LockedClass:
+    """
+    Defining a list with all attrs you want to use
+    Anything not in the list cannot be used as attrs
+    """
+    __slot__ = ["first_name"]
