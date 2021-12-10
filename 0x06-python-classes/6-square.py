2a34475164a77c89fc6885ec2d4ac52e6c41ddf9
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-10 21:02:29 +0000

    Adding new instance attr position to print whitespaces before #

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -80,7 +80,7 @@ class Square:
         It will validate if it is an int or greater/eq than 0
         """
         if type(value) is not tuple or len(value) != 2 or \
-           type(value[:]) is not int or value[:] < 0:
+           type(value[0:]) is not int or value[0:] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
