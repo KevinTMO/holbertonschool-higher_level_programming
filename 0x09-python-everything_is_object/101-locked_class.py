9e6ce7e9416754ef1867c94f272c0e9850a3796f
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-11 15:22:01 +0000

    Fixing type on __slots__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,13 +7,12 @@ Except when calling "first_name"
 """
 
 
-class LockedClass:
+class LockedClass():
     """
     Defining a list with all attrs you want to use
     Anything not in the list cannot be used as attrs
     """
-
-    __slot__ = ["first_name"]
+    __slots__ = 'first_name'
 
     def __init__(self, name=""):
         self.first_name = name
