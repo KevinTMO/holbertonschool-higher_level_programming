6818e84f68ced2c630d74f36c58ef49697f52410
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-11 03:12:37 +0000

    Using staticmethod decorator

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -135,6 +135,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
         Return the biggest area of the two rectangles after compare
