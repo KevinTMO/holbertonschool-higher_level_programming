d50bd1d002b09b01b69d8d8a0b0df9ab95b520ea
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-11 02:14:29 +0000

    Returnint a string rep of an object

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+"""
+Module 4-rectangle.py
+
+Create a __repr__() method that returns a str rep of the object
+
+Class:
+- Rectangle:
+
+Methods:
+- __init__()
+- def width(self) / def width(self, value)
+- def height(self) / def height(self, value)
+- def area(self)
+- def perimeter(self)
+   - if width or height == 0 then perimeter is == 0
+- def __str__(self)
+- def __repr__(self)
+
+Decorators:
+- @property
+- @setter
+"""
+
+
+class Rectangle:
+    """
+    Will get the width/height of a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initiate objects with attrs width/height
+        Assign each attr with a value
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Getter of width attr to return the value
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter of width attr will set width with a value
+        Validate if width is int or is greater/eq than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter of heigth attr to return the value
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter of height attr will set height with a value
+        Validate if height is int or is greater/eq than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        Return the area of a rectangle using height + width
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Return the perimeter of a rectangle using heigth + width
+        if height or width are == 0 then perimeter is 0
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+        Return a string rep of rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        else:
+            return ('\n'.join('#' * self.__width for rc in
+                              range(self.__height)))
+
+    def __repr__(self):
+        """
+        Return a string rep of the object
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
