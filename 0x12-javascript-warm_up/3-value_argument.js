0a0ef8db757cead4490d32e165d7e0515ed0e2cf
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-13 17:07:10 +0000

    Using arg explicitly

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const argv = require('process').argv;
-const firstArgv = argv[2];
 
 if (argv.length < 3) {
   console.log('No argument');
 } else {
-  console.log(firstArgv);
+  console.log(argv[2]);
 }
