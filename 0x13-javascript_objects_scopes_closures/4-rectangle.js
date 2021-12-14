92a1796d754769d95f056c8d5c22020b3f14bbf2
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-14 16:21:02 +0000

    Adding two methods: rotate/double ; rotate will exchange values between height/width && double will multiply their value by 2

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+  let str = '';
+    for (let row = 1; row <= this.height; row++) {
+      for (let col = 1; col <= this.width; col++) {
+        str += 'X';
+      }
+      if (row < this.height) {
+	str += '\n';
+      }
+    }
+    console.log(str);
+    str = '';
+  }
+
+  rotate () {
+    let tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+
+};
+
+/*
+ ANOTHER WAY TO DO THIS
+  if (w <= 0 && h <= 0) {
+    this.width = {};
+    this.height = {};
+  }
+*/
