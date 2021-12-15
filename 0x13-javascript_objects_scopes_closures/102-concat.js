b0fc1edf4bcdf824942676680b8b07b4e3401e9e
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-15 21:21:24 +0000

    Concatinating two files content to another

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const argv = require('process').argv;
+const fs = require('fs');
+
+const fileA = argv[2];
+const fileB = argv[3];
+const fileC = argv[4];
+
+const textA = fs.readFileSync(fileA, 'UTF-8');
+const textB = fs.readFileSync(fileB, 'UTF-8');
+
+
+console.log(fileC);
+fs.writeFileSync(fileC, textA + textB);
