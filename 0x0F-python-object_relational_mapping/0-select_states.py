914da132cbe6d00adbb53b661e563e382a0dcd7d
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-17 22:46:45 +0000

    Less hardcode using argv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 import MySQLdb
+from sys import argv
 
-cnndb = MySQLdb.connect(host='localhost', user='root', passwd='', db='hbtn_0e_0_usa', charset='utf8')
+cnndb = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf8')
 
 cur = cnndb.cursor();
 cur.execute('SELECT * FROM states ORDER BY id ASC')
