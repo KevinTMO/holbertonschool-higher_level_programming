211644a1798d69d62663e509a91700751862d508
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-18 00:14:56 +0000

    Select fields from two tables and group them by ocurrences of states_id

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+cnndb = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf8')
+
+cur = cnndb.cursor()
+cur.execute('SELECT cities.id, cities.name, states.name FROM cities, states WHERE states.id = state_id')
+
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+cnndb.close()
