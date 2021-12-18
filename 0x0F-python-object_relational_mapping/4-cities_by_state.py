c031de4eb9ea66e01053583746e24eca8521c3fb
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-18 16:02:09 +0000

    Adding documentation, safeguard and fixing identation

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
+"""
+Write a script that filter ocurrences of cities in states by id
+"""
+
 
 import MySQLdb
 from sys import argv
 
-cnndb = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf8')
+if __name__ == "__main__":
 
-cur = cnndb.cursor()
-cur.execute('SELECT cities.id, cities.name, states.name FROM cities, states WHERE states.id = state_id')
+    cnndb = MySQLdb.connect(host='localhost',
+                            port=3306,
+                            user=argv[1],
+                            passwd=argv[2],
+                            db=argv[3],
+                            charset='utf8')
 
-rows = cur.fetchall()
+    cur = cnndb.cursor()
+    cur.execute('SELECT cities.id, cities.name, states.name FROM cities,\
+    states WHERE states.id = state_id')
 
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
 
-cur.close()
-cnndb.close()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    cnndb.close()
