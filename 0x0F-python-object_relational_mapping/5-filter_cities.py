7909e41dc7e71271f8474eecde00221d6f6d55aa
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-18 16:04:54 +0000

    Adding documentation, safeguard, and fixing identation

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
+"""
+Write a script that return the cities associated of a state they give
+"""
+
 
 import MySQLdb
 from sys import argv
 
-state_name = argv[4]
+if __name__ == "__main__":
 
-cnndb = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf8')
+    state_name = argv[4]
 
-cur = cnndb.cursor()
-cur.execute('SELECT cities.name FROM cities, states WHERE states.name = %(state_name)s AND state_id = states.id',{'state_name': state_name})
+    cnndb = MySQLdb.connect(host='localhost',
+                            port=3306,
+                            user=argv[1],
+                            passwd=argv[2],
+                            db=argv[3],
+                            charset='utf8')
 
-rows = cur.fetchall()
-cities = ()
+    cur = cnndb.cursor()
+    cur.execute('SELECT cities.name FROM cities, states\
+    WHERE states.name = %(state_name)s\
+    AND state_id = states.id',{'state_name': state_name})
 
-for row in rows:
-    cities += row
-print(', '.join(cities))
+    rows = cur.fetchall()
+    cities = ()
 
-cur.close()
-cnndb.close()
+    for row in rows:
+        cities += row
+    print(', '.join(cities))
+
+    cur.close()
+    cnndb.close()
