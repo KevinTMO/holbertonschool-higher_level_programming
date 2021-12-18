adf03297d74cc00948d1f4ffbdaadbcef11fbe95
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-18 15:31:14 +0000

    Fixing pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,7 +3,12 @@
 import MySQLdb
 from sys import argv
 
-cnndb = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf8')
+cnndb = MySQLdb.connect(host='localhost',
+                        port=3306,
+                        user=argv[1],
+                        passwd=argv[2],
+                        db=argv[3],
+                        charset='utf8')
 
 cur = cnndb.cursor()
 cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
