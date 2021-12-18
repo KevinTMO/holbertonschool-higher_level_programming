420a633cf6feccc16114ccdd53f0cbf38edcfa25
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-18 19:18:48 +0000

    Adding a new statement to secure output

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,20 +8,21 @@ from sys import argv
 
 if __name__ == "__main__":
 
+    state_arg = argv[4]
     cnndb = MySQLdb.connect(host='localhost',
                             port=3306,
                             user=argv[1],
                             passwd=argv[2],
-                            db=argv[3])
+                            db=argv[3],
+                            charset='utf8')
 
     cur = cnndb.cursor()
     cur.execute('SELECT * FROM states WHERE name LIKE "{:s}%" ORDER BY id ASC'.
                 format(argv[4]))
 
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
+    for row in cur.fetchall():
+        if row[1] == state_arg:
+            print(row)
 
     cur.close()
     cnndb.close()
