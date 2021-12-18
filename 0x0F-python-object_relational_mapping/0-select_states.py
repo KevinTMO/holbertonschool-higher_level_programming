9ffc7191344fafae3f9ef2005be5f1c0cc04c323
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-18 15:08:16 +0000

    Adding safeguard to not executed module when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     cnndb = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset='utf8')
 
     cur = cnndb.cursor();
-    cur.execute('SELECT * FROM states ORDER BY id ASC')
+    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
 
     rows = cur.fetchall()
 
