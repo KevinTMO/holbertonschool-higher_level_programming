9210b150d4500d8ddbfd9d82f9e6b90d710ac695
KevinTMO <kevin.ramos3@hotmail.com>
2021-12-19 02:20:29 +0000

    Filter the content inside table by char 'a' inside the field 'name'

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Display only the entires that contains the letter 'a'
+"""
+
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    User = argv[1]
+    Passwd = argv[2]
+    Db = argv[3]
+    Host = "localhost"
+
+    Engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                           format(User, Passwd, Host, Db))
+
+    Session = sessionmaker(bind=Engine)
+    session = Session()
+    output = session.query(State).filter(State.name.like == ('a%')).\
+    order_by(State.id)
+
+    for rows in output:
+        print("{}: {}".format(rows.id, rows.name))
+
+    session.close()
