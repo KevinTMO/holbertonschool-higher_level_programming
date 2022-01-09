4303fae775adece57f14c8965c990ef4cef0e6a6
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-09 08:18:13 +0000

    Making a request to get whatever catch_me got rewrite the domain using Origin

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Making a request to get whatever catch_me got rewrite the domain using Origin
-curl -sL -d "You got me!" -X "PUT" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sL -d "user_id=98" -X "PUT" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
