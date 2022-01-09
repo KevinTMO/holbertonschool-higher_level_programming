d1be1c210804e668dc55b2fbe642990c1d37baee
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-09 07:39:34 +0000

    Sending 2 vars with POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sending two variables and requesting a POST
-curl -sX POST -H "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
