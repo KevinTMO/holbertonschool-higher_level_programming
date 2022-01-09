43d5a58fdb99f31cc677e9230ecae79d14161e17
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-09 18:45:55 +0000

    Fetching from an url using urllib-request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,9 +12,9 @@ Write a Python script that fetches https://intranet.hbtn.io/status
 
 if __name__ == '__main__':
 
-    from urllib.request import urlopen
+    import urllib.request
 
-    with urlopen('https://intranet.hbtn.io/status') as File:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as File:
         html = File.read()
         print('Body response:')
         print('    - type: {}'.format(type(html)))
