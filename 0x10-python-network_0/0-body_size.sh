dfefd011f7adb589942f6e9c64ef5e58839e41dc
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-09 05:29:47 +0000

    Get the size of the content body using grep | awk

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Connect to the url, then get the Content-Length in the response header
-curl -s $1 --include | grep -i Content-Length | awk '{print $2}'
+curl -s "$1" --include | grep -i Content-Length | awk '{print $2}'
