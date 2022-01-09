c42af0a768aefb3b9c76e4523072ac54b2677e8b
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-09 18:34:48 +0000

    Fetching from an url using urllib-request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches https://intranet.hbtn.io/status
+
+- You must use the package urllib
+- You are not allowed to import any packages other than urllib
+- The body of the response must be displayed
+  like the following example (tabulation before -)
+- You must use a with statement
+"""
+
+
+from urllib.request import urlopen
+
+
+if __name__ == '__main__':
+
+    with urlopen('https://intranet.hbtn.io/status') as File:
+        html = File.read()
+        print('Body response:')
+        print('    - type: {}'.format(type(html)))
+        print('    - content: {}'.format(html))
+        print('    - utf8 content: {}'.format(html.decode('utf-8')))
