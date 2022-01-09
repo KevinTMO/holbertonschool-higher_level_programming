dfb078c90222ee4396ab35b9ff5157c8027367d2
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-09 21:48:15 +0000

    Fetching from requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -14,7 +14,7 @@ from requests import get
 
 if __name__ == '__main__':
 
-    with get('https://intranet.hbtn.io/status') as File:
-        print('Body response:')
-        print('\t- type: {}'.format(type(File.text)))
-        print('\t- content: {}'.format(File.text))
+    response = get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
