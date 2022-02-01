8e4574a97ccd93d1796c2fa3b51e240eae660120
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-01 13:39:59 +0000

    Writing data into a file and parsin err

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/*
+Write into a file and process it to utf-8
+If there's an error print the error object
+then print the data
+*/
+
+const fs = require('fs')
+
+const file = process.argv[2]
+const data = process.argv[3]
+
+fs.writeFile(file, data, 'utf-8', function(err) {
+    if (err) {
+	console.error(err);
+	return;
+    }
+});
+
+/*
+
+-------- USING FAST FUNCTION PARSING ARGS --------
+
+const fs = require('fs')
+
+const file = process.argv[2]
+const data = process.argv[3]
+
+fs.writeFile(file, data, 'utf-8', (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+});
+
+*/
